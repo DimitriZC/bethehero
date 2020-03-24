a1219d109a6ea204811e75f01eccf9a702d7df4d
Dimitri <dimitrizuave14@gmail.com>
2020-03-24 20:00:28 -0300

    Ready to configure DB

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,37 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/Listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+ /**
+  * Tipos de parâmetros:
+  * 
+  * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, Paginação) ==> req.query
+  * Route Params: Parâmetros utilizados para identificar recursos ==> req.params
+  * Request Body: Corpo da requiseção, utilizado para criar ou alterar recursos
+  */
+
+  /**
+   * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+   * NoSQL: MongoDB, CouchDB, ...
+   */
+
+app.post('/users', (req, res) => {
+  const body = req.body;
+
+  console.log(body);
+
   return res.json({
-    evente: 'Semana Omnistack 11.0 ===',
+    evento: 'Semana Omnistack 11.0',
     aluno: 'Dimitri Zuave'
   });
 });
