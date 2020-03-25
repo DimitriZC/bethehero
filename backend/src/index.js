5d2cbf4ce0663a9e212badba8e98cdca673bfb27
Dimitri <dimitrizuave14@gmail.com>
2020-03-25 09:41:22 -0300

    Back-end done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,45 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const routes = require('./routes')
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-/**
- * Métodos HTTP:
- * 
- * GET: Buscar/Listar uma informação do back-end
- * POST: Criar uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Deletar uma informação no back-end
- */
-
- /**
-  * Tipos de parâmetros:
-  * 
-  * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, Paginação) ==> req.query
-  * Route Params: Parâmetros utilizados para identificar recursos ==> req.params
-  * Request Body: Corpo da requiseção, utilizado para criar ou alterar recursos
-  */
-
-  /**
-   * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
-   * NoSQL: MongoDB, CouchDB, ...
-   */
-
-   /**
-    * Driver: SELECT * FROM users
-    * Query Builder: table('users').select('*').where()
-    */
-
-app.post('/users', (req, res) => {
-  const body = req.body;
-
-  console.log(body);
-
-  return res.json({
-    evento: 'Semana Omnistack 11.0',
-    aluno: 'Dimitri Zuave'
-  });
-});
+app.use(routes);
 
 app.listen(3333)
